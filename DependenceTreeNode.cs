2c2b8b656036fdec46db6c5189cab5ae14b3f6a1
romansalakhov <salakhov.roman12@gmail.com>
2019-11-11 18:38:18 +0500

    Added field "Priority" in class "DependenceTreeNode"

--- DependenceTreeNode.cs
+++ DependenceTreeNode.cs
@@ -9,12 +9,15 @@ namespace ScheduleTask2
         public List<DependenceTreeNode> InDependeces { get; set; }
 
         public DependenceTreeNode OutDependence { get; set; }
+        
+        public int Priority { get; set; }
 
         public DependenceTreeNode(Job job)
         {
             this.Job = job;
             InDependeces = new List<DependenceTreeNode>();
             OutDependence = null;
+            Priority = -1;
         }
     }
 }
