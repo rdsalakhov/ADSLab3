e966853eb3e353480bb906a5a8109b4f628a4633
romansalakhov <salakhov.roman12@gmail.com>
2019-11-11 21:24:24 +0500

    Added "CreateTreeFromFile" method to the class "Program".

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ScheduleTask2
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ScheduleTask2
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            var tree = CreateTreeFromFile("input.txt");
+            tree.SetPriorities();
+            var chart = new GanttChart(3);
+            chart.Fill(tree);
+        }
+
+        static DependenceTree CreateTreeFromFile(string path)
+        {
+            var streamReader = new StreamReader(path);
+
+            int jobCount = int.Parse(streamReader.ReadLine());
+
+            int dependenceCount = int.Parse(streamReader.ReadLine());
+
+            var dependenceDict = new Dictionary<char, char>();
+
+            for (int i = 0; i < dependenceCount; i++)
+            {
+                var dependencePair = streamReader.ReadLine().Split(' ');
+
+                dependenceDict.Add(char.Parse(dependencePair[0]), char.Parse(dependencePair[1]));
+            }
+
+            return DependenceTreeCreator.CreateFromDependenceDictionary(dependenceDict, jobCount);
         }
     }
 }
