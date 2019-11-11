c22715fab29116df8da064ce822125742c194b38
romansalakhov <salakhov.roman12@gmail.com>
2019-11-11 12:23:24 +0500

    Added class "Job"

--- DependenceTreeNode.cs
+++ DependenceTreeNode.cs
@@ -2,7 +2,6 @@ namespace ScheduleTask2
 {
     public class DependenceTreeNode
     {
-        public int hui = 1;
-        public int huiPobolshe = 10;
+        
     }
 }
--- Job.cs
+++ Job.cs
@@ -5,7 +5,17 @@ namespace ScheduleTask2
     public class Job
     {
         private char jobNamer = 'A';
-        public char job;
-        public List
+
+        public char Name { get; }
+
+        public Job()
+        {
+            if (jobNamer == 'Z')
+            {
+                throw new Exception("Unable creating new job. Job limit achieved.");
+            }
+            Name = jobNamer;
+            jobNamer++;
+        }
     }
 }
