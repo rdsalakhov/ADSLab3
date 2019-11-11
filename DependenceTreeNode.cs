689b3e09ca339817b4945ee4398e29697b856955
romansalakhov <salakhov.roman12@gmail.com>
2019-11-11 12:33:41 +0500

    Added class "DependenceTreeNode"

--- DependenceTreeNode.cs
+++ DependenceTreeNode.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
+
 namespace ScheduleTask2
 {
     public class DependenceTreeNode
     {
-        
+        public Job Job { get; }
+
+        public List<DependenceTreeNode> InDependeces { get; set; }
+
+        public DependenceTreeNode OutDependence { get; set; }
+
+        public DependenceTreeNode(Job job)
+        {
+            this.Job = job;
+            InDependeces = new List<DependenceTreeNode>();
+            OutDependence = null;
+        }
     }
 }
