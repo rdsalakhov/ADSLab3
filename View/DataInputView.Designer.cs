d0b6e9a9145e82f8f44465e8a6ca812f89b99b4f
Arseny Neutroev <neustroev.arseny@gmail.com>
2019-11-19 23:08:56 +0500

    Implemented DataInputView with controller to send data to model

---
+++ View/DataInputView.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace View
+{
+    partial class DataInputView
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tasksNumberTextField = new System.Windows.Forms.TextBox();
+            this.empNumberTextField = new System.Windows.Forms.TextBox();
+            this.leftNodeBox = new System.Windows.Forms.ComboBox();
+            this.rightNodeBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addDependencyButton = new System.Windows.Forms.Button();
+            this.dependenciesList = new System.Windows.Forms.ListBox();
+            this.removeDependencyButton = new System.Windows.Forms.Button();
+            this.computeButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14F);
+            this.label1.Location = new System.Drawing.Point(115, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of tasks:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14F);
+            this.label2.Location = new System.Drawing.Point(115, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(401, 43);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of employees:";
+            // 
+            // tasksNumberTextField
+            // 
+            this.tasksNumberTextField.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tasksNumberTextField.Location = new System.Drawing.Point(581, 114);
+            this.tasksNumberTextField.Name = "tasksNumberTextField";
+            this.tasksNumberTextField.Size = new System.Drawing.Size(164, 50);
+            this.tasksNumberTextField.TabIndex = 2;
+            this.tasksNumberTextField.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.LimitTextToNumbers);
+            this.tasksNumberTextField.Leave += new System.EventHandler(this.TasksNumberFilled);
+            // 
+            // empNumberTextField
+            // 
+            this.empNumberTextField.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.empNumberTextField.Location = new System.Drawing.Point(581, 209);
+            this.empNumberTextField.Name = "empNumberTextField";
+            this.empNumberTextField.Size = new System.Drawing.Size(164, 50);
+            this.empNumberTextField.TabIndex = 3;
+            this.empNumberTextField.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.LimitTextToNumbers);
+            // 
+            // leftNodeBox
+            // 
+            this.leftNodeBox.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftNodeBox.FormattingEnabled = true;
+            this.leftNodeBox.Location = new System.Drawing.Point(188, 374);
+            this.leftNodeBox.Name = "leftNodeBox";
+            this.leftNodeBox.Size = new System.Drawing.Size(145, 50);
+            this.leftNodeBox.TabIndex = 4;
+            // 
+            // rightNodeBox
+            // 
+            this.rightNodeBox.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightNodeBox.FormattingEnabled = true;
+            this.rightNodeBox.Location = new System.Drawing.Point(548, 374);
+            this.rightNodeBox.Name = "rightNodeBox";
+            this.rightNodeBox.Size = new System.Drawing.Size(145, 50);
+            this.rightNodeBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14F);
+            this.label3.Location = new System.Drawing.Point(412, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 43);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "=>";
+            // 
+            // addDependencyButton
+            // 
+            this.addDependencyButton.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addDependencyButton.Location = new System.Drawing.Point(264, 448);
+            this.addDependencyButton.Name = "addDependencyButton";
+            this.addDependencyButton.Size = new System.Drawing.Size(339, 55);
+            this.addDependencyButton.TabIndex = 7;
+            this.addDependencyButton.Text = "Add dependency";
+            this.addDependencyButton.UseVisualStyleBackColor = true;
+            this.addDependencyButton.Click += new System.EventHandler(this.addConnectionButton_Click);
+            // 
+            // dependenciesList
+            // 
+            this.dependenciesList.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dependenciesList.FormattingEnabled = true;
+            this.dependenciesList.ItemHeight = 42;
+            this.dependenciesList.Location = new System.Drawing.Point(77, 560);
+            this.dependenciesList.Name = "dependenciesList";
+            this.dependenciesList.Size = new System.Drawing.Size(397, 298);
+            this.dependenciesList.TabIndex = 8;
+            // 
+            // removeDependencyButton
+            // 
+            this.removeDependencyButton.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeDependencyButton.Location = new System.Drawing.Point(523, 653);
+            this.removeDependencyButton.Name = "removeDependencyButton";
+            this.removeDependencyButton.Size = new System.Drawing.Size(259, 103);
+            this.removeDependencyButton.TabIndex = 9;
+            this.removeDependencyButton.Text = "Remove dependency";
+            this.removeDependencyButton.UseVisualStyleBackColor = true;
+            this.removeDependencyButton.Click += new System.EventHandler(this.removeDependencyButton_Click);
+            // 
+            // computeButton
+            // 
+            this.computeButton.Font = new System.Drawing.Font("Arial", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computeButton.Location = new System.Drawing.Point(252, 907);
+            this.computeButton.Name = "computeButton";
+            this.computeButton.Size = new System.Drawing.Size(361, 103);
+            this.computeButton.TabIndex = 10;
+            this.computeButton.Text = "Compute!";
+            this.computeButton.UseVisualStyleBackColor = true;
+            this.computeButton.Click += new System.EventHandler(this.computeButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14F);
+            this.label4.Location = new System.Drawing.Point(274, 302);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(329, 43);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Input dependency:";
+            // 
+            // DataInputView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 1047);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.computeButton);
+            this.Controls.Add(this.removeDependencyButton);
+            this.Controls.Add(this.dependenciesList);
+            this.Controls.Add(this.addDependencyButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rightNodeBox);
+            this.Controls.Add(this.leftNodeBox);
+            this.Controls.Add(this.empNumberTextField);
+            this.Controls.Add(this.tasksNumberTextField);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DataInputView";
+            this.Text = "Data input view";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tasksNumberTextField;
+        private System.Windows.Forms.TextBox empNumberTextField;
+        private System.Windows.Forms.ComboBox leftNodeBox;
+        private System.Windows.Forms.ComboBox rightNodeBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button addDependencyButton;
+        private System.Windows.Forms.ListBox dependenciesList;
+        private System.Windows.Forms.Button removeDependencyButton;
+        private System.Windows.Forms.Button computeButton;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ View/DataInputView.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class DataInputView : Form
+    {
+
+        List<Tuple<char, char>> dependensies = new List<Tuple<char, char>>();
+        public DataInputView()
+        {
+            InitializeComponent();
+        }
+
+        private void LimitTextToNumbers(object sender, KeyPressEventArgs args)
+        {
+            args.Handled = !char.IsDigit(args.KeyChar) & !char.IsControl(args.KeyChar);
+        }
+
+        private void TasksNumberFilled(object sender, EventArgs e)
+        {
+            int alphabetLength = 25;
+            
+            int numberOfTasks;
+            if (!int.TryParse(tasksNumberTextField.Text, out numberOfTasks) || numberOfTasks > alphabetLength)
+            {
+                MessageBox.Show($"The value {tasksNumberTextField.Text} is improper. Try to fill it with an integer lower or equal to {alphabetLength}");
+                numberOfTasks = 0;
+                tasksNumberTextField.Text = "";
+                tasksNumberTextField.Focus();
+                return;
+            }
+            UpdateNodeBoxes(numberOfTasks);
+        }
+
+        private void UpdateNodeBoxes(int numberOfTasks)
+        {
+            int alphabetOffSet = 65; // offset to convert byte to char
+
+            leftNodeBox.Items.Clear();
+            rightNodeBox.Items.Clear();
+            for (int i = 0; i < numberOfTasks; i++)
+            {
+                leftNodeBox.Items.Add((char)(i+alphabetOffSet));
+                rightNodeBox.Items.Add((char)(i+alphabetOffSet));
+            }
+        }
+
+        private void addConnectionButton_Click(object sender, EventArgs e)
+        {
+            if (rightNodeBox.SelectedItem == null || leftNodeBox.SelectedItem == null)
+            {
+                MessageBox.Show("One or more jobs were not selected in the checkboxes. Try selecting both!");
+                return;
+            }
+
+            if (rightNodeBox.SelectedItem.ToString() == leftNodeBox.SelectedItem.ToString())
+            {
+                MessageBox.Show("Unable to add dependency between simmilar job. Try choosing another job in one of the comboboxes");
+                return;
+            }
+            Tuple<char, char> pairToInput = new Tuple<char, char>(leftNodeBox.SelectedItem.ToString()[0], rightNodeBox.SelectedItem.ToString()[0]);
+            if (dependensies.Contains(pairToInput))
+            {
+                MessageBox.Show("Unable to add dependency that is already in the list!");
+                return;
+            }
+            dependensies.Add(new Tuple<char, char>(pairToInput.Item1, pairToInput.Item2));
+            UpdateDependencyList();
+        }
+
+        private void UpdateDependencyList()
+        {
+            dependenciesList.Items.Clear();
+            foreach(Tuple<char, char> keyValue in dependensies)
+            {
+                dependenciesList.Items.Add(keyValue.Item1.ToString() + " -> " + keyValue.Item2.ToString());
+            }
+        }
+
+        private void removeDependencyButton_Click(object sender, EventArgs e)
+        {
+            if (dependenciesList.SelectedItem == null)
+            {
+                MessageBox.Show("No dependency selected to delete. Firstly choose one from the list");
+                return;
+            }
+            string stringFromList = dependenciesList.SelectedItem.ToString();
+            Tuple<char, char> valueToDelete = new Tuple<char, char>(stringFromList[0], stringFromList[stringFromList.Length-1]);
+            dependensies.Remove(valueToDelete);
+            UpdateDependencyList();
+        }
+
+        private void computeButton_Click(object sender, EventArgs e)
+        {
+            int numberOfEmployees;
+            if (!int.TryParse(empNumberTextField.Text, out numberOfEmployees))
+            {
+                MessageBox.Show($"Unable to convert {empNumberTextField.Text} to integer for NUMBER OF EMPLOYEES. Try again!");
+                return;
+            }
+            if (dependensies.Count == 0)
+            {
+                MessageBox.Show("There are no dependencies in the list");
+                return;
+            }
+            ComputeWithData(numberOfEmployees, dependensies);
+        }
+
+        private void ComputeWithData(int numberOfEmployees, List<Tuple<char, char>> dependecies)
+        {
+            Console.WriteLine("Data is sent!");
+            Console.WriteLine($"Number of workers - {numberOfEmployees}");
+            foreach(var dependecy in dependecies)
+            {
+                Console.WriteLine(dependecy.Item1.ToString() + " " + dependecy.Item2.ToString());
+            }
+        }
+    }
+}
